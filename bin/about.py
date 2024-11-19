ed807f0a2ec8beae8603010f654beb8ab6b22f7e
Uziel <urlaweblink@gmail.com>
2024-11-18 21:52:49 -0500

    updated the Abouttab() to display

--- bin/about.py
+++ bin/about.py
@@ -1,10 +1,11 @@
 from ipywidgets import Output
 from IPython.display import display, HTML
-
+import os
 class AboutTab(object):
-
     def __init__(self):
-        # self.tab = Output(layout={'height': '600px'})
         self.tab = Output(layout={'height': 'auto'})
-        self.tab.append_display_data(HTML(filename='doc/about.html'))
-        
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        about_html_path = os.path.abspath(os.path.join(base_path, '../doc/about.html'))
+        if not os.path.isfile(about_html_path):
+            raise FileNotFoundError(f"No such file or directory: '{about_html_path}'")
+        self.tab.append_display_data(HTML(filename=about_html_path))

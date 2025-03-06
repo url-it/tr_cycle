05198195eddd892981f5a05bca475740b2b21282
Uziel <urlaweblink@gmail.com>
2025-03-06 17:23:45 -0500

    Switching from precomplied

--- bin/Cycle_Training.py
+++ bin/Cycle_Training.py
@@ -430,8 +430,7 @@ def run_button_cb(s):
 
         run_button.description = "WAIT..."
         # CHANGED HERE FOR LOCAL MYPROJ
-        myprojPath = os.path.abspath("../bin/myproj")
-        process = subprocess.Popen([myprojPath, "config.xml"],
+        process = subprocess.Popen(["../bin/myproj", "config.xml"],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE,
                                    universal_newlines=True)

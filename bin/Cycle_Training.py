e86f01f128a4e8c02a861b7969bcb1844e9d2bbd
Uziel <uzrivera@iu.edu>
2025-04-29 01:33:18 -0400

    Adding precomp

--- bin/Cycle_Training.py
+++ bin/Cycle_Training.py
@@ -386,6 +386,17 @@ def outcb(s):
         sub.update()
     return s
 
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+myprojPath = find("myproj", os.getcwd())
+if myprojPath:
+    subprocess.run(["chmod", "+x", myprojPath], check=True)
+    print(f"Executable permissions granted for: {myprojPath}")
+else:
+    print("Error: 'myproj' file not found in the specified path.")
 
 # Callback for the ("dumb") 'Run' button (without hublib.ui)
 def run_button_cb(s):
@@ -430,7 +441,8 @@ def run_button_cb(s):
 
         run_button.description = "WAIT..."
         # CHANGED HERE FOR LOCAL MYPROJ
-        process = subprocess.Popen(["../bin/myproj", "config.xml"],
+        path = "/content/tr_cycle/bin/myproj"
+        process = subprocess.Popen([path, "config.xml"],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE,
                                    universal_newlines=True)

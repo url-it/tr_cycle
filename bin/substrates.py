fb0eaeaf0c44ba36b358c1440389680413925075
Uziel <uzrivera@iu.edu>
2025-04-29 01:52:03 -0400

    Checking

--- bin/substrates.py
+++ bin/substrates.py
@@ -641,7 +641,8 @@ class SubstrateTab(object):
         with zipfile.ZipFile('svg.zip', 'w') as myzip:
             for f in glob.glob(file_str):
                 myzip.write(f, os.path.basename(f))   # 2nd arg avoids full filename path in the archive
-        # plt.ion()
+        plt.close('all')
+
 
         if self.colab_flag:
             files.download('svg.zip')
